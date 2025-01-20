04d277d65da636f12cb461132841cbb63db59a94
nk <narminelkilani@gmail.com>
2025-01-20 10:55:03 +0100

    added NOT NULL conditions

--- dump.sql
+++ dump.sql
@@ -12,36 +12,35 @@ DROP TABLE IF EXISTS propose CASCADE;
 
 CREATE TABLE composant_electrique (
     libelle varchar(20) PRIMARY KEY,
-    nom_composant varchar(30),
+    nom_composant varchar(30) NOT NULL,
     description text,
     marque varchar(30)
 );
 
 CREATE TABLE client (
     num_client serial PRIMARY KEY,
-    nom_client varchar(50),
-    prenom_client varchar(50),
-    adresse_email_client varchar(50) UNIQUE,
-    mot_de_passe_client varchar(100)
+    nom_client varchar(50) NOT NULL,
+    prenom_client varchar(50) NOT NULL,
+    adresse_email_client varchar(50) UNIQUE
 );
 
 CREATE TABLE projet (
     id_projet serial PRIMARY KEY,
-    nom_projet varchar(40),
+    nom_projet varchar(40) NOT NULL,
     num_client int REFERENCES client(num_client)
 );
 
 CREATE TABLE personnel (
     id_personnel serial PRIMARY KEY,
-    nom_personnel varchar(50),
-    prenom_personnel varchar(50),
-    mot_de_passe_personnel varchar(100)
+    nom_personnel varchar(50) NOT NULL,
+    prenom_personnel varchar(50) NOT NULL,
+    mot_de_passe     varchar(50) NOT NULL
 );
 
 CREATE TABLE magasin (
     num_magasin serial PRIMARY KEY,
     adresse_magasin varchar(50),
-    nom_magasin varchar(20),
+    nom_magasin varchar(20) NOT NULL,
     num_telephone_magasin varchar(50),
     gerant int REFERENCES personnel(id_personnel)
 );
@@ -67,7 +66,7 @@ CREATE TABLE compose (
     libelle varchar(20) references  composant_electrique(libelle),
     id_transaction serial references transaction(id_transaction),
     quantite_produit int,
-    prix_unitaire_achat real,
+    prix_unitaire_initial real,
     primary key (libelle, id_transaction)
 );
 
@@ -90,34 +89,42 @@ CREATE TABLE propose(
 );
 
 
-
-
-
-
+---Filling table composant_electrique :
 
 INSERT INTO composant_electrique VALUES
 ('PB00622154','Fox Spirit FG1 (Black)',
-'Medium Tower case with tempered glass panel and 3 ARGB 120 mm fans on the front', 'Fox Spirit'),
+'Medium Tower case with tempered glass panel and 3 ARGB 120 mm 
+fans on the front', 'Fox Spirit'),
 ('PB00390708', 'ASRock A520M Phantom Gaming 4', 
 'Micro-ATX motherboard Socket AM4 AMD A520 - 4x DDR4 -
  SATA 6Gb/s M.2 PCI-E NVMe - USB 3.0 - 
 2x PCI-Express 3.0 16x', 'ASRock');
 
-INSERT INTO client(nom_client, prenom_client, adresse_email_client, mot_de_passe_client) VALUES 
-('Kita', 'Etienne', 'etienne@mail.com', '123456'), 
-('Gon', 'Freecss', 'Gon@strong.com'), ('Zoldik', 'Killua', 'Zoldyck@assasin.com', 'GonMDP');
+---Filling table client :
+
+INSERT INTO client(nom_client, prenom_client, adresse_email_client) VALUES 
+('Kita', 'Etienne', 'etienne@mail.com'), 
+('Gon', 'Freecss', 'Gon@strong.com'), ('Zoldik', 'Killua', 'Zoldyck@assasin.com');
+
+---Filling table projet :
 
 INSERT INTO projet(nom_projet, num_client) VALUES 
 ('Chimera Ant Desctruction', 2), 
 ('PC Building', 3);
 
-INSERT INTO personnel(nom_personnel, prenom_personnel, mot_de_passe_personnel) VALUES 
-('Freecss', 'Ging', '123456'),
- ('Monkey.D', 'Luffy', 'LuffyMDP');
+---Filling table personnel :
+
+INSERT INTO personnel(nom_personnel, prenom_personnel, mot_de_passe) VALUES 
+('Freecss', 'Ging', '084fe8aecafea8b2f84'),
+ ('Monkey.D', 'Luffy', '084fe8afufa8b2f84');
+
+---Filling table magasin :
 
 INSERT INTO magasin(adresse_magasin, nom_magasin, num_telephone_magasin, gerant) VALUES 
 ('50 rue Carthage', 'Cabsa', '07877777779', 1), 
 ('12 rue Byrsa','Byrsa' ,'0656348313', 2);
+
+---Filling table transaction :
 
 INSERT INTO transaction(date_transaction, num_client, num_magasin, id_personnel) VALUES 
 ('13-03-2022', 1, 1, 1), 
@@ -149,4 +156,4 @@ INSERT INTO propose VALUES
 ('PB00622154', 1 , 82.95),
 ('PB00622154', 2 , 89.99 ),
 ('PB00390708', 1 , 83.60),
-('PB00390708', 2 , NULL);
+('PB00390708', 2 , 80);
